94ebb414653be56f7bdef00e35d42de6a913f8e4
Wjosedev <wjose.dev@gmail.com>
2024-06-08 10:40:14 -0300

    Atualização3

--- Documents/Cursos/Portifólio/Flask/aula/views.py
+++ Documents/Cursos/Portifólio/Flask/aula/views.py
@@ -1,8 +1,15 @@
 from aula import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    nome = 'Willams José'
+    idade = 44
+    interesses = ['Python', 'flask', 'Desenvolvimento Weg']
+    return render_template('index.html', nome=nome, idade=idade, interesses =interesses) 
+    
 
+@app.route('/Contato/')
+def novapagina():
+    return 'outra view'
 
